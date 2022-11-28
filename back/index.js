fd9835f791160764cfa3da13366762cf7a3b4e6c
madhuri434 <madhuri1.ranvirkar@gmail.com>
2022-11-27 18:16:55 -0800

    backend changes to index.js,package-lock.json, routes/flights.js

--- back/index.js
+++ back/index.js
@@ -4,18 +4,18 @@ import cassandra from 'cassandra-driver';
 import cors from 'cors';
 
 import flightRoutes from './routes/flight.js';
-
+//const flightRoutes = requiere('./routes/flight.js');
 
 const app = express();
 
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
 app.use('/api', flightRoutes);
-const PORT = 6000;
+const PORT = 8080;
 
 const client = new cassandra.Client({
   contactPoints: ['127.0.0.1'],
@@ -24,13 +24,14 @@ const client = new cassandra.Client({
 });
 
 client.connect()
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
-    const Mapper = cassandra.mapping.Mapper;
+const Mapper = cassandra.mapping.Mapper;
 
 const mapper = new Mapper(client, {
-    models: {'flights': {
+  models: {
+    'flights': {
       tables: ['flightdetails'],
       columns: {
         'dayofmonth': 'dayofmonth',
@@ -39,11 +40,12 @@ const mapper = new Mapper(client, {
         'originairportid': 'originairportid',
         'destairportid': 'destairportid'
       }
-    }}
-  })
+    }
+  }
+})
 
 const flightsMapper = mapper.forModel('flights');
 
 export {
-    flightsMapper 
+  flightsMapper
 };
--- back/routes/flight.js
+++ back/routes/flight.js
@@ -1,11 +1,18 @@
+// import express from 'express';
+
+import { getFlights } from '../controller/flight.js'
+
+// const router = express.Router();
+
+// router
+//     .route("/:flight")
+//     .get(getFlights);
+
+// export default router;
+
+//const express = require("express");
 import express from 'express';
-
-import {getFlights} from '../controller/flight.js'
-
 const router = express.Router();
-
-router
-    .route("/:flight")
-    .get(getFlights);
-
+router.get("/:flight", getFlights);
+// module.exports = router;
 export default router;
